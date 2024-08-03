8902dc65fc610dd64226c0118ca5bee13400314b
abrar997 <abraralrawi997.com>
2024-08-03 16:59:52 +0300

    nav responsive

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("bar");
+  const navMenu = document.getElementById("nav");
+  const close = document.getElementById("close");
+  if (button) {
+    button.addEventListener("click", function () {
+      navMenu.classList.replace("-right-[300px]", "right-0");
+    });
+  }
+
+  if (close) {
+    close.addEventListener("click", function () {
+      navMenu.classList.replace("right-0", "-right-[300px]");
+    });
+  }
+});
